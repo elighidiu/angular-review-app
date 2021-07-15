7654d3d61655bed46d239c823d1f7aed2008ee91
Your Nameeli <elighidiu@yahoo.com>
2021-07-15 11:22:05 +0200

    created class review

--- reviewapp/src/app/app.component.ts
+++ reviewapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Review } from './review';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
+  //create new obj reviewModel through the review class with all the properties set to null.
+ reviewModel = new Review("", "", "", "");
 }
---
+++ reviewapp/src/app/review.ts
@@ -0,0 +1,6 @@
+export class Review {
+ constructor(public name:String, 
+            public email:String,
+            public image:String,
+            public textarea:String) {}
+}
